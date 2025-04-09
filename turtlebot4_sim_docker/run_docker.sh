8c3b6dd8df5e3f88e8bf9b8e6e7a05ed73fbf0cd
Jack Gordon <72707894+jgordon314@users.noreply.github.com>
2025-04-09 14:38:18 -0400

    Update run_docker.sh

--- turtlebot4_sim_docker/run_docker.sh
+++ turtlebot4_sim_docker/run_docker.sh
@@ -8,7 +8,7 @@ docker run -it --rm --net=host \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v /dev/snd:/dev/snd \
   -v <PUT_PATH_TO_REPO_HERE>/TIC_Software_Challenge_S25:/TIC_Software_Challenge_S25 \
-  -w <PUT_PATH_TO_REPO_HERE>/TIC_SoftwareChallenge_S25 \
+  -w /TIC_SoftwareChallenge_S25 \
   turtlebot4_test:latest \
   # You will need to change the path to your home directory in lines 10 and 11
   # You can get the path to your TIC_Software_Challenge_S25 folder by running the command:
