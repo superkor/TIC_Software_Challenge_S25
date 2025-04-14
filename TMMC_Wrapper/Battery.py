bd33ff730e88c19abdfa407010c43c4363781be1
mbridger2027 <mtpbridg@uwaterloo.ca>
2025-04-14 12:39:27 -0400

    Update Battery.py

--- TMMC_Wrapper/Battery.py
+++ TMMC_Wrapper/Battery.py
@@ -1,8 +1,11 @@
 import rclpy
 
 class Battery:
-    @staticmethod
-    def checkBattery(robot):
-        robot.battery_state_future = rclpy.Future()
-        robot.spin_until_future_completed(robot.battery_state_future)
-        return robot.last_battery_state_msg.percentage
+    def __init__(self, robot):
+        self.robot = robot
+
+    def checkBattery(self):
+        self.robot.battery_state_future = rclpy.Future()
+        self.robot.spin_until_future_completed(self.robot.battery_state_future)
+        return self.robot.last_battery_state_msg.percentage
+
