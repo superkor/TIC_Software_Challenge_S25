7432aa9e1be672cb88ecd420d4c20cbd12644b6c
mbridger2027 <mtpbridg@uwaterloo.ca>
2025-04-14 12:34:10 -0400

    Update __init__.py

--- TMMC_Wrapper/__init__.py
+++ TMMC_Wrapper/__init__.py
@@ -1,6 +1,7 @@
 # include all files in this folder
 from .Battery import Battery
 from .Camera import Camera
+from .Constants import Constants
 from .IMU import IMU
 from .Lidar import Lidar
 from .Logging import Logging
@@ -8,7 +9,9 @@ from .Control import Control
 from .Robot import Robot
 
 
+  
 __all__ = [
+    "Constants",
     "Battery",
     "Camera",
     "Control",
