589e069a4327d89ae8909f4f1b14eb4c3db1331f
mbridger2027 <mtpbridg@uwaterloo.ca>
2025-04-16 12:27:54 -0400

    Update Lidar.py

--- TMMC_Wrapper/Lidar.py
+++ TMMC_Wrapper/Lidar.py
@@ -8,29 +8,6 @@ class Lidar:
         self.robot.spin_until_future_completed(self.robot.scan_future)
         return self.robot.last_scan_msg
 
-    def lidar_data_too_close(self, scan, th1, th2, min_dist):
-        #returns points between angles th1, th2 that are closer tha min_dist
-        if th2 < th1:
-            temp = th1
-            th1 = th2 
-            th2 = temp 
-        th1 = max(th1, scan.angle_min)
-        th2 = min(th2, scan.angle_max)
-        ind_start = int((th1-scan.angle_min)/scan.angle_increment)
-        ind_end = int((th2-scan.angle_min)/scan.angle_increment)
-        meas = scan.ranges[ind_start:ind_end]
-        total = len(meas)
-        meas = [m for m in meas if np.isfinite(m)]
-        if len(meas) == 0:
-            return 0.0
-        num_too_close = 0.0
-        for m in meas: 
-            if m < min_dist: 
-                print(f"m < min dist addition is {m}")
-                num_too_close = num_too_close + 1
-
-        return float(num_too_close) / total
-
     def detect_obstacle_in_cone(self, scan, distance, center, offset_angle):
         obstacle_dist = distance
 
