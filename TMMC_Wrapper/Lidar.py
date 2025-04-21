a279d4e2a72bee4eb4bb2a8aaaa23df002211612
jgordon314 <jgordon1482@gmail.com>
2025-04-21 19:58:46 +0000

    fixed lidar cone detection for sim

--- TMMC_Wrapper/Lidar.py
+++ TMMC_Wrapper/Lidar.py
@@ -13,48 +13,50 @@ class Lidar:
 
     def detect_obstacle_in_cone(self, scan, distance, center, offset_angle) -> tuple[float, float]:
         ''' Analyzes the scan data within the cone to detect and return the distance and angle of the closest obstacle. '''
-        obstacle_dist = distance
-
+        if offset_angle < 0 or offset_angle > 180:
+            raise ValueError("offset_angle must be between 0 and 180 degrees")
+        
         if scan is None:
             print("Warning: No LIDAR Data")
             return -1,-1
-        
-        if offset_angle < 0 or offset_angle >= 180:
-            print("Warning: Offset angle must be between 0 and 180 degrees.")
-            return -1, -1
-        
+                
         # Need to seperate the logic for simulation and real robot
         # because the angles are different in the two cases.
         # In simulation, the angles are in degrees and range from 0 to 360.
         # In the real robot, the angles are in degrees and range from 0 to 720.
         if self.robot.IS_SIM:
             # Find the relevent range
-            left = center + offset_angle
+            while center >= 360:
+                center = center - 360
+            while center < 0:
+                center = center + 360
+            
             right = center - offset_angle
+            left = center + offset_angle
 
-            if right < 0:
-                right = right + 360
             if left >= 360:
                 left = left - 360
+            if right < 0:
+                right = right + 360
 
             # Get the smallest distance in the relevent range
-            if right > left:
-                relevent_1 = scan.ranges[0:(left + 1)]
+            if right == left:
+                relevent_range = scan.ranges[0:360]
+                min_dist = min(relevent_range)
+                min_dist_index = relevent_range.index(min_dist)
+            elif right > left:
+                relevent_1 = scan.ranges[0:left]
                 relevent_2 = scan.ranges[right: 360]
                 relevent_range = relevent_1 + relevent_2
-
                 min_dist = min(relevent_range)
                 min_dist_index = relevent_range.index(min_dist)
-
-                if min_dist_index > left:
-                    min_dist_angle = left - min_dist_index
-                else:
-                    min_dist_angle = min_dist_index
             else:
-                relevent_range = scan.ranges[right:(left + 1)]
+                relevent_range = scan.ranges[right:left]
                 min_dist = min(relevent_range)
                 min_dist_index = relevent_range.index(min_dist)
-                min_dist_angle = min_dist_index + right
+            min_dist_angle = min_dist_index
+            if min_dist_angle > 180:
+                min_dist_angle = min_dist_angle - 360
         else:
             # Find the relevent range
             new_center = center + 180
@@ -93,7 +95,7 @@ class Lidar:
                 min_dist_index = relevent_range.index(min_dist)
                 min_dist_angle = (min_dist_index + right) / 2 - 90
 
-        if min_dist <= obstacle_dist:
+        if min_dist <= distance:
             return min_dist, min_dist_angle
         
         # If no obstacle is detected, return -1 for both distance and angle
